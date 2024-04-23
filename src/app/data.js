ab601ca1aeb2288fd2385acd70dfdcff6db75a9d
SR Rony <146630569+SR-Rony@users.noreply.github.com>
2024-04-23 07:25:54 +0600

    portfolio all setup

---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Wildlife photography",
+    description: "Track your carbon footprint",
+    date: "2022-08-15",
+    demoLink: "https://photography-jet.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Chatting Application",
+    description: "Digital art showcase platform",
+    date: "2022-06-20",
+    demoLink: "https://chatting-application-six.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Fast Jaw",
+    description: "Plan and track expenses",
+    date: "2022-09-10",
+    demoLink: "https://fast-jao.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "Orebi Ecommerce",
+    description: "Monitor heart rate zones",
+    date: "2022-05-30",
+    demoLink: "https://orebi-ecommerce-sigma.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "Linkdin",
+    description: "Discover new recipes",
+    date: "2022-07-12",
+    demoLink: "https://linkedin-three-brown.vercel.app/",
+  },
+  {
+    id: 6,
+    name: "Pack & Go",
+    description: "Log your travels",
+    date: "2022-10-01",
+    demoLink: "https://packgo.vercel.app/",
+  },
+  {
+    id: 7,
+    name: "Hockey Training",
+    description: "Collaborative learning platform",
+    date: "2022-04-18",
+    demoLink: "https://hockey-six.vercel.app/",
+  },
+  {
+    id: 8,
+    name: "TechTalk",
+    description: "Tech news aggregator",
+    date: "2022-11-05",
+    demoLink: "https://techtalk.example.com",
+  },
+  {
+    id: 9,
+    name: "FitTrack",
+    description: "Fitness and workout tracker",
+    date: "2022-03-22",
+    demoLink: "https://fittrack.example.com",
+  },
+  {
+    id: 10,
+    name: "Hockey Training",
+    description: "Meditation and mindfulness app",
+    date: "2022-02-14",
+    demoLink: "https://hockey-six.vercel.app/",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/SR-Rony",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/sr-rony",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://twitter.com/SRRony707",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
